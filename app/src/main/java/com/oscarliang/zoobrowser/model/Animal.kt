279301f1ddc88c24eafc92e0175863e9d1b5ae0f
OscarLiang <a7757729@gmail.com>
2024-03-31 17:23:47 +0800

    Add animal model and item layout

---
+++ app/src/main/java/com/oscarliang/zoobrowser/model/Animal.kt
@@ -0,0 +1,18 @@
+package com.oscarliang.zoobrowser.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "animals")
+data class Animal(
+    @PrimaryKey
+    @SerializedName("_id")
+    val id: Int,
+    @SerializedName("a_name_ch")
+    val name: String,
+    @SerializedName("a_feature")
+    val feature: String,
+    @SerializedName("a_pic01_url")
+    val imageUrl: String
+)
