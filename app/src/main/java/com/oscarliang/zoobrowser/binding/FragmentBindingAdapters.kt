dbc48af099312a4ef125cf261c01fe1ce8c2f080
OscarLiang <a7757729@gmail.com>
2024-04-02 18:05:27 +0800

    Add network_security_config to fix glide loading error

--- app/src/main/java/com/oscarliang/zoobrowser/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/com/oscarliang/zoobrowser/binding/FragmentBindingAdapters.kt
@@ -4,13 +4,19 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.oscarliang.zoobrowser.R
+import java.lang.StringBuilder
 
 class FragmentBindingAdapters {
 
     @BindingAdapter(value = ["imageUrl"])
     fun bindImage(imageView: ImageView, url: String?) {
+        // Convert http to https
+        var formatUrl = url
+        if (url?.get(4) != 's') {
+            formatUrl = StringBuilder(url).apply { insert(4, 's') }.toString()
+        }
         Glide.with(imageView.context)
-            .load(url)
+            .load(formatUrl)
             .placeholder(R.drawable.ic_zoo_gray)
             .error(R.drawable.ic_error)
             .into(imageView)
